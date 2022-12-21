db2af97f0a3a8f2e2ccc9b7f8f15b15100843281
Kanad Gupta <git@kanad.dev>
2022-12-20 17:03:34 -0800

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+# Remove git-tracked copies and symlink them with existing copies
+# (so changes can be reverted piecemeal)
+
+# zshrc
+echo "Regenerating/linking .zshrc"
+rm ./zsh/.zshrc
+ln ~/.zshrc ./zsh/.zshrc
+
+# gitconfig
+echo "Regenerating/linking .gitconfig"
+rm ./.gitconfig
+ln ~/.gitconfig ./.gitconfig
+
+echo "Done!"
